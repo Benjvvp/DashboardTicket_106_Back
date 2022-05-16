78280fdd4b949743f12b93f6821b01c4a782e382
Benjvvp <bperez51511a@gmail.com>
2022-05-16 14:33:45 -0400

    replace dates

--- src/utils/logsSystem.ts
+++ src/utils/logsSystem.ts
@@ -6,17 +6,25 @@ export function pushLogInFile(log: string) {
     const date = new Date();
     const dateString = date.toLocaleDateString();
     const timeString = date.toLocaleTimeString();
+
     if (!fs.existsSync(path.join(__dirname, `../logs`))) {
       fs.mkdirSync(path.join(__dirname, `../logs`));
     }
 
-    const file = fs.createWriteStream(
-      path.join(__dirname, `../logs/logs-${dateString}.txt`),
-      { flags: "a" }
-    );
-    file.write(`${timeString} - ${log}\n`);
-    file.end();
-    
+    if (
+      !fs.existsSync(
+        path.join(__dirname, `../logs/${dateString.replace("/", "-")}`)
+      )
+    ) {
+      fs.mkdirSync(
+        path.join(__dirname, `../logs/${dateString.replace("/", "-")}`)
+      );
+    } else {
+      fs.appendFileSync(
+        path.join(__dirname, `../logs/${dateString.replace("/", "-")}/log.txt`),
+        `${timeString} - ${log}\n`
+      );
+    }
   } catch (error) {
     console.log(error);
   }
