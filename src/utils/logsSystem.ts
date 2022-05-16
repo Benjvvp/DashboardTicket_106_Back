103bc8fe02f72968c63aeb8f92d9193f4e002ed5
Benjvvp <bperez51511a@gmail.com>
2022-05-16 14:14:19 -0400

    logsSystem try and catch

--- src/utils/logsSystem.ts
+++ src/utils/logsSystem.ts
@@ -1,14 +1,18 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
-export function pushLogInFile(log: string){
-      const date = new Date();
-      const dateString = date.toLocaleDateString();
-      const timeString = date.toLocaleTimeString();
-      const logFile = path.join(__dirname, `../../../logs/${dateString}-log.txt`);
-      if(!fs.existsSync(logFile)){
-            fs.writeFileSync(logFile, `${dateString} ${timeString} - ${log}`);
-      }else{
-            fs.appendFileSync(logFile, `\n${dateString} ${timeString} - ${log}`);
-      }
+export function pushLogInFile(log: string) {
+  try {
+    const date = new Date();
+    const dateString = date.toLocaleDateString();
+    const timeString = date.toLocaleTimeString();
+    const logFile = path.join(__dirname, `../../../logs/${dateString}-log.txt`);
+    if (!fs.existsSync(logFile)) {
+      fs.writeFileSync(logFile, `${dateString} ${timeString} - ${log}`);
+    } else {
+      fs.appendFileSync(logFile, `\n${dateString} ${timeString} - ${log}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
