87101bd7ffeea3eab431b484fdf9a17369bc199f
Benjvvp <bperez51511a@gmail.com>
2022-05-16 14:39:37 -0400

    logs System in beta

--- src/utils/logsSystem.ts
+++ src/utils/logsSystem.ts
@@ -7,24 +7,7 @@ export function pushLogInFile(log: string) {
     const dateString = date.toLocaleDateString();
     const timeString = date.toLocaleTimeString();
 
-    if (!fs.existsSync(path.join(__dirname, `../logs`))) {
-      fs.mkdirSync(path.join(__dirname, `../logs`));
-    }
-
-    if (
-      !fs.existsSync(
-        path.join(__dirname, `../logs/${dateString.replace("/", "-")}`)
-      )
-    ) {
-      fs.mkdirSync(
-        path.join(__dirname, `../logs/${dateString.replace("/", "-")}`)
-      );
-    } else {
-      fs.appendFileSync(
-        path.join(__dirname, `../logs/${dateString.replace("/", "-")}/log.txt`),
-        `${timeString} - ${log}\n`
-      );
-    }
+    console.log(`${dateString} ${timeString} - ${log}`);
   } catch (error) {
     console.log(error);
   }
